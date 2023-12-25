5d8ca49ac66df914c0848b473fe7738543a2570a
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2023-12-25 17:43:47 +0900

    Time: 31 ms (98.89%) | Memory: 17.3 MB (5.06%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,6 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        del nums1[m: m + n]
-        nums1.extend(nums2)
-        nums1.sort()
+        idx1, idx2, insert_idx = m - 1, n - 1, m + n - 1
+
+        while idx2 >= 0:
+            if idx1 >= 0 and nums1[idx1] > nums2[idx2]:
+                nums1[insert_idx] = nums1[idx1]
+                idx1 -= 1
+            else:
+                nums1[insert_idx] = nums2[idx2]
+                idx2 -= 1
+            insert_idx -= 1
