aaa3f8343ec45c1e6cd8fcc2b1aa035baddf039e
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2024-01-26 00:19:02 -0800

    Time: 362 ms (15.54%) | Memory: 147.9 MB (53.39%) - LeetSync

--- 35-search-insert-position/search-insert-position.dart
+++ 35-search-insert-position/search-insert-position.dart
@@ -2,9 +2,9 @@ class Solution {
   int searchInsert(List<int> nums, int target) {
     int low = 0;
     int high = nums.length - 1;
-    int mid = (high - low) ~/ 2 + low;
 
     while (low <= high) {
+      int mid = (high - low) ~/ 2 + low;
       if (nums[mid] == target) {
         return mid;
       } else if (nums[mid] > target) {
@@ -12,7 +12,6 @@ class Solution {
       } else if (nums[mid] < target) {
         low = mid + 1;
       }
-    mid = (high - low) ~/ 2 + low;
     }
 
     return low;
